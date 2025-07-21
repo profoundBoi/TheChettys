c8dc723560320e02a0657f9ecbe8dffebc697272
profoundBoi <connor170103@gmail.com>
2025-07-21 22:41:17 +0200

    last push

--- docs/rsvp/rsvp.js
+++ docs/rsvp/rsvp.js
@@ -2,7 +2,7 @@
 const { createClient } = supabase;
 
 const SUPABASE_URL = "https://kucidicpvzfbhzcfqxer.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1Y2lkaWNwdnpmYmh6Y2ZxeGVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxMDg2MzcsImV4cCI6MjA2ODY4NDYzN30.wnRNF2a4xbQLsMstq9hNO9lgOs_xEw3l3mzDfyI3itQ";
 
 const client = createClient(SUPABASE_URL, SUPABASE_KEY); //  Now using 'client'
 
